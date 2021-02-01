5ab059be3890404634b73d634936fe26c786f479
Julia  Martins  Maschion <julia.maschion@ifood.com.br>
2021-01-31 21:52:32 -0300

    wip

--- rocketshoes/src/pages/Home/index.js
+++ rocketshoes/src/pages/Home/index.js
@@ -1,54 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/15/HZM-1729-715/HZM-1729-715_zoom2.jpg?ts=1583429518&ims=326x"
-          alt="Tênis feminino rosa"
-        />
-        <strong>Tênis feminino Nike</strong>
-        <span>R$ 200,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FFF" />3
-          </div>
-        <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/15/HZM-1729-715/HZM-1729-715_zoom2.jpg?ts=1583429518&ims=326x"
-          alt="Tênis feminino rosa"
-        />
-        <strong>Tênis feminino Nike</strong>
-        <span>R$ 200,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FFF" />3
-          </div>
-        <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/15/HZM-1729-715/HZM-1729-715_zoom2.jpg?ts=1583429518&ims=326x"
-          alt="Tênis feminino rosa"
-        />
-        <strong>Tênis feminino Nike</strong>
-        <span>R$ 200,00</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="FFF" />3
-          </div>
-        <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="FFF" />3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
